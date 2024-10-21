503a8bc0252040b5c68f2fca07ed7a23f17b869a
Hodlevskyj <vt211_goo@student.ztu.edu.ua>
2024-10-21 16:37:00 +0300

    зроблено два методи: додавання товару та відображення списку

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -3,34 +3,49 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\MakerBundle\Str;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Request;
 
-
+#[Route('/item')]
 class TestController extends AbstractController
 {
-    #[Route('/test', name: 'app_test_get',methods:["GET"])]
+    public array $items=[];
+    #[Route('/get', name: 'app_item_get',methods:["GET"])]
     public function get(Request $request): JsonResponse
     {
         // return $this->render('test/index.html.twig', [
         //     'controller_name' => 'TestController',
         // ]);
-        $queryParams = $request->query->all();
+//        $queryParams = $request->query->all();
         // $data=['sf','rer','dsf'];
-        return new JsonResponse($queryParams,200);
+//        return new JsonResponse($queryParams,200);
+//        return  new JsonResponse($this->items,200);
+        $queryParams = $request->query->all();
+        return new JsonResponse($queryParams, 200);
     }
 
 
-    #[Route('/test-post', name: 'app_test_post',methods:["POST"])]
+    #[Route('/post', name: 'app_item_post',methods:["POST"])]
     public function post(Request $request): JsonResponse
     {
         // return $this->render('test/index.html.twig', [
         //     'controller_name' => 'TestController',
         // ]);
         // getContent()
-        $requestParams = $request->request->all();
+//        $requestParams = $request->request->all();
         // $data=['sf','rer','dsf'];
-        return new JsonResponse($requestParams);
+//        return new JsonResponse($requestParams);
+        $requestBody=json_decode($request->getContent(), true);
+        return new JsonResponse($requestBody, 200);
     }
+
+    #[Route('/get-item/{id}', name: 'app_get_item',methods:["GET"])]
+    public function getItem(string $id): JsonResponse
+    {
+
+        return new JsonResponse($id, 200);
+    }
+
 }
